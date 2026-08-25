ac3f378bf870c824e27266f297d15d0a252f84e9
PanicGecko <adamjava123.ac@gmail.com>
2026-08-25 14:59:14 -0400

    Boiler Code

---
+++ project1/models/user.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+
+class User(ABC):
+
+    def __init__(self, pin, owner_id=None):
+        self._pin = pin
+        self._owner_id = owner_id
+
+    def check_pin(self, pin):
+        return pin == self._pin
+
+    def change_pin(self, old_pin, new_pin):
+        if not self.check_pin(old_pin):
+            raise ValueError("Invalid old PIN")
+
+        self._pin = new_pin
+        return True
+
+    def get_owner_id(self):
+        return self._owner_id
+
+    @abstractmethod
+    def get_role(self):
+        pass
