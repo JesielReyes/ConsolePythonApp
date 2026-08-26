d504587105b3f793910b12241a406d03bc405a6b
Jesiel Reyes <144545348+JesielReyes@users.noreply.github.com>
2026-08-26 14:29:57 -0400

    resturcures our database to follow standards agreed upon

--- project1/database.py
+++ project1/database.py
@@ -2,7 +2,8 @@ import os #os is used to access our environement variables, in this case we are 
 from typing import Annotated #this is used to create a type hint for our session dependency, which is used in our routers to access the database session. Basically a shortcut to avoid having to write the same code over and over again in our routers
 
 from fastapi import Depends #this is going to tell fastapi that we want to use the results from another function before we run the function we are currently in. We will be using it to get our database session before running our routers
-from sqlmodel import Session, SQLModel, create_engine #we use sessions to communicate with the database, SQLModel is used to define the base of our metadata, and create_engine creates a connection manager for our database
+from sqlmodel import Session, SQLModel, create_engine
+from sqlalchemy import text
 
 from models.account_model import AccountRecord #imports the account table model we made so SQLModel knows the layout when creating our database tables
 
@@ -13,13 +14,21 @@ from models.account_model import AccountRecord #imports the account table model 
 #     raise RuntimeError(
 #         "DATABASE_URL must be set before starting the application"
 #     )
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///banking.db")
 #an engine is just a connection manager. It will know what database to connect to, which driver to use, how to open the connections for the databse, and how to manage and reuse those connections.
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+)
 
 #only used if the tables dont currently exist in the database
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+    if not DATABASE_URL.startswith("sqlite"):
+        with engine.begin() as connection:
+            connection.execute(text(
+                "ALTER TABLE transaction ALTER COLUMN category DROP NOT NULL"
+            ))
 
 #a session is a temp connection to the database, used to query records, add records, update, etc
 def get_session():
