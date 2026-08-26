200bfcfb908fd47dc5efa472a1f77098d5cd85c7
PanicGecko <adamjava123.ac@gmail.com>
2026-08-26 09:48:33 -0400

    Changed Account Repo

--- project1/repository/accountsRepo.py
+++ project1/repository/accountsRepo.py
@@ -1,15 +1,16 @@
 from sqlmodel import Session, select
+from models.account_model import AccountRecord
 
 def get_user_account_by_number(session: Session, user_id: int, account_num: int):
-    statement = select(Account).where(
-        Account.account_num == account_num,
-        Account.user_id == user_id
+    statement = select(AccountRecord).where(
+        AccountRecord.account_number == account_num,
+        AccountRecord.owner_id == user_id
     )
 
     return session.exec(statement).first()
 
 def get_account_by_number(session: Session, account_num: int):
-    statement = select(Account).where(
-        Account.account_num == account_num
+    statement = select(AccountRecord).where(
+        AccountRecord.account_number == account_num
     )
     return session.exec(statement).first()
